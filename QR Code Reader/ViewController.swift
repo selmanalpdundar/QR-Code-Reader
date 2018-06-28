aeffc4fc1fefdb322ae40c46673f347955e5e244
selman <selman.alp@hotmail.com.tr>
2018-06-28 15:06:47 +0300

    Initial Commit

---
+++ QR Code Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QR Code Reader
+//
+//  Created by Selman on 6/28/18.
+//  Copyright © 2018 Selman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
